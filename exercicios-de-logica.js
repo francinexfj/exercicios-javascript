6d1a19e8c4990a3e11e3588e7fe40c7f329418b7
Francine <francinexfj@gmail.com>
2021-10-22 18:43:44 +0200

    exercicios 5 e 6

--- exercicios-de-logica.js
+++ exercicios-de-logica.js
@@ -35,7 +35,19 @@ while(multiplicando <= 10){
 
 // 5 Soma dos números de 1 a 10
 
+var soma = 0;
+for(var i = 0;i <= 10;i++){
+	soma = soma + i;
+}
+console.log(soma);
+
 // 6 Calculado o fatorial de 10
+
+var fatorial = 1;
+for(var i = 1;i <= 10; i++){
+	fatorial *= i;
+}
+console.log(fatorial);
 
 // 7 Criar uma função para calcular a porcentagem de um número
 
